311c7a71f7a280269948008e5cf9a1221fd43746
Anish <anishsajueralil0@gmail.com>
2021-08-24 15:22:01 +0530

    nav bar is implemented

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint ,render_template
 
 views = Blueprint("views","__name__")
-@views.route("/home")
+@views.route("/")
 def home():
-    return render_template("home.html",name="anish")
+    return render_template("home.html")
